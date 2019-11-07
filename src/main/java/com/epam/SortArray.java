efde1082d975c9146803c7d4bf9311813089143c
Andriy <asoha474@gmail.com>
2019-11-08 01:33:11 +0200

    add printing odd numbers method

--- src/main/java/com/epam/SortArray.java
+++ src/main/java/com/epam/SortArray.java
@@ -1,4 +1,16 @@
 package com.epam;
 
+
 public class SortArray {
+    
+    public static void printOdd(int[] array) {
+        for (int i : array) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+
+
 }
