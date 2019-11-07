22ae2c63368916e2b6a5a253964b59029a0edf45
Andriy <asoha474@gmail.com>
2019-11-08 01:42:09 +0200

    add reverse printing even numbers method

--- src/main/java/com/epam/SortArray.java
+++ src/main/java/com/epam/SortArray.java
@@ -2,7 +2,7 @@ package com.epam;
 
 
 public class SortArray {
-    
+
     public static void printOdd(int[] array) {
         for (int i : array) {
             if (i % 2 != 0) {
@@ -11,6 +11,13 @@ public class SortArray {
         }
     }
 
+    public static void printEvenReverse(int[] array) {
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+    }
 
 
 }
