e8b9450d69117656599b6b9f2e8c6e793fb07edd
Andriy <asoha474@gmail.com>
2019-11-07 23:49:27 +0200

    get Fibonacci sequence instead of printing it

--- src/main/java/com/epam/Fibonacci.java
+++ src/main/java/com/epam/Fibonacci.java
@@ -1,17 +1,19 @@
 package com.epam;
 
+
 public class Fibonacci {
 
-    public static void printSequence(int size) {
+    public static long[] getSequence(int size) {
         long num1 = 0;
         long num2 = 1;
         long temp;
-        System.out.println("First " + size + " fibonacci numbers:");
+        long[] sequence = new long[size];
         for (int i = 1; i <= size; i++) {
-            System.out.print(num1 + " ");
             temp = num1 + num2;
             num1 = num2;
             num2 = temp;
+            sequence[i-1] = num1;
         }
+        return sequence;
     }
 }
