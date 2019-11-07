08ed41f5b2c853f440e9d4f014a64354bef14b1a
Andriy <asoha474@gmail.com>
2019-11-08 01:10:25 +0200

    add SortArray class for array processing

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -1,4 +1,21 @@
 package com.epam;
 
+import java.util.Arrays;
+
 public class Application {
+
+    static private long[] asd;
+
+    public static void main(String[] args) {
+//        asd = Fibonacci.getSequence(3);
+//                System.out.println("First " + 111 + " fibonacci numbers:");
+//        System.out.println(Arrays.toString(asd));
+//
+//        System.out.println(Fibonacci.getBiggestOdd(asd));
+//        System.out.println(Fibonacci.getBiggestEven(asd));
+//        System.out.println(Fibonacci.getPercentageOdd(asd));
+//        System.out.println(Fibonacci.getPercentageEven(asd));
+
+
+    }
 }
