8606b88baf1769f6e57e339886adc5b8151feb3b
Andriy <asoha474@gmail.com>
2019-11-08 00:01:34 +0200

    add getting biggest odd number method

--- src/main/java/com/epam/Fibonacci.java
+++ src/main/java/com/epam/Fibonacci.java
@@ -12,8 +12,20 @@ public class Fibonacci {
             temp = num1 + num2;
             num1 = num2;
             num2 = temp;
-            sequence[i-1] = num1;
+            sequence[i - 1] = num1;
         }
         return sequence;
     }
+
+    public static long getBiggestOdd(long[] array) {
+        long max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if ((array[i] > max) && (array[i] % 2 != 0)) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+
 }
