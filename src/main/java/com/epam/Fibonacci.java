c00923332ba6cd6adc9ee4946576b0a29d682a1d
Andriy <asoha474@gmail.com>
2019-11-07 22:36:23 +0200

    change Fibonacci class : add a Fibonacci sequence printing method

--- src/main/java/com/epam/Fibonacci.java
+++ src/main/java/com/epam/Fibonacci.java
@@ -1,4 +1,17 @@
 package com.epam;
 
 public class Fibonacci {
+
+    public static void printSequence(int size) {
+        long num1 = 0;
+        long num2 = 1;
+        long temp;
+        System.out.println("First " + size + " fibonacci numbers:");
+        for (int i = 1; i <= size; i++) {
+            System.out.print(num1 + " ");
+            temp = num1 + num2;
+            num1 = num2;
+            num2 = temp;
+        }
+    }
 }
