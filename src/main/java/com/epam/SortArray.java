640620649ba2153610eb2a5bbad72839844cdf41
Andriy <andriymattt@gmail.com>
2019-11-09 18:19:42 +0200

    add Javadoc comments

--- src/main/java/com/epam/SortArray.java
+++ src/main/java/com/epam/SortArray.java
@@ -1,11 +1,25 @@
 package com.epam;
 
+/**
+ * Used for arrays processing
+ */
 public class SortArray {
 
+    /**
+     * Prevents calls from a subclass, because this is utility class
+     * and it should not have a public or default constructor
+     *
+     * @throws UnsupportedOperationException unsupported operation exception
+     */
     protected SortArray() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Finds and prints the odd numbers of array
+     *
+     * @param array an array of integers
+     */
     public static void printOdd(int[] array) {
         for (int i : array) {
             if (i % 2 != 0) {
@@ -14,6 +28,11 @@ public class SortArray {
         }
     }
 
+    /**
+     * Finds and prints the even numbers of array by reverse
+     *
+     * @param array an array of integers
+     */
     public static void printEvenReverse(int[] array) {
         for (int i = array.length - 1; i >= 0; i--) {
             if (array[i] % 2 == 0) {
@@ -22,6 +41,11 @@ public class SortArray {
         }
     }
 
+    /**
+     * Calculates and prints the sum of odd numbers of array
+     *
+     * @param array an array of integers
+     */
     public static void printSumOdd(int[] array) {
         int sum = 0;
         for (int i : array) {
@@ -32,6 +56,11 @@ public class SortArray {
         System.out.println(sum);
     }
 
+    /**
+     * Calculates and prints the sum of even numbers of array
+     *
+     * @param array an array of integers
+     */
     public static void printSumEven(int[] array) {
         int sum = 0;
         for (int i : array) {
